07bd756c3a1189230bfc45f74f3b5b822d77373d
rucra <rucrazia@naver.com>
2020-03-10 23:35:40 +0900

    메인화면 디자인 변경

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:InstagramWeatherFlutter/src/pages/image_list_page.dart';
 import 'src/pages/home_page.dart';
 import 'src/theme/theme.dart';
 import 'src/pages/main_home.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -11,6 +13,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', 'US'), // English
+        const Locale('kr', 'KR'), // Thai
+      ],
 
       theme: AppTheme.lightTheme,
       home: MainHome(),
