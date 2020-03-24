874a0bbfea73df425004079953a4627ba9ff51b3
rucra <rucrazia@naver.com>
2020-03-24 21:22:21 +0900

    second screen(home screen) - ing

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -13,85 +13,178 @@ class HomePage extends StatelessWidget {
     return ClipRRect(
       borderRadius: BorderRadius.only(
           bottomLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
-      child: Container(
-          height: 200,
-          width: width,
-          decoration: BoxDecoration(
-            color: LightColor.purple,
+      child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 200,
+                width: width,
+                decoration: BoxDecoration(
+                  color: LightColor.purple,
+                ),
+                child: Stack(
+                  fit: StackFit.expand,
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Positioned(
+                        top: 30,
+                        right: -100,
+                        child: _circularContainer(300, LightColor.lightpurple)),
+                    Positioned(
+                        top: -100,
+                        left: -45,
+                        child: _circularContainer(width * .5, LightColor.darkpurple)),
+                    Positioned(
+                        top: -180,
+                        right: -30,
+                        child: _circularContainer(width * .7, Colors.transparent,
+                            borderColor: Colors.white38)),
+                    Positioned(
+                        top: 40,
+                        left: 0,
+                        child: Container(
+                            width: width,
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Icon(
+                                  Icons.keyboard_arrow_left,
+                                  color: Colors.white,
+                                  size: 40,
+                                ),
+                                SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: <Widget>[
+                                    Text(
+                                      "지역을 입력해주세요.",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    IconButton(
+                                      icon: Icon(Icons.search),
+                                      tooltip: 'Search',
+                                      onPressed: () => showMessage('IconButton'),
+                                    ),
+                                    FloatingActionButton(
+                                        child:Icon(Icons.print),
+                                        onPressed: () => showDialog(
+                                            context:context,
+                                            builder:(context) {
+                                              return AlertDialog(content: Text(myController.text));
+                                            }
+                                        )
+                                    )
+                                  ],
+                                ),
+                                SizedBox(height: 0), //여백
+                                TextField(
+                                    controller: myController,
+                                    decoration: InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: '샌프란시스코'
+                                    ),
+                                    style: TextStyle(
+                                        color: Colors.white54,
+                                        fontSize: 30,
+                                        fontWeight: FontWeight.w500)
+                                ),
+                              ],
+                            )))
+                  ],
+                )
+
+          )
           ),
-          child: Stack(
-            fit: StackFit.expand,
-            alignment: Alignment.center,
-            children: <Widget>[
-              Positioned(
-                  top: 30,
-                  right: -100,
-                  child: _circularContainer(300, LightColor.lightpurple)),
-              Positioned(
-                  top: -100,
-                  left: -45,
-                  child: _circularContainer(width * .5, LightColor.darkpurple)),
-              Positioned(
-                  top: -180,
-                  right: -30,
-                  child: _circularContainer(width * .7, Colors.transparent,
-                      borderColor: Colors.white38)),
-              Positioned(
-                  top: 40,
-                  left: 0,
-                  child: Container(
-                      width: width,
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Icon(
-                            Icons.keyboard_arrow_left,
-                            color: Colors.white,
-                            size: 40,
-                          ),
-                          SizedBox(height: 10),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Text(
-                                "지역을 입력해주세요.",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                              IconButton(
-                                icon: Icon(Icons.search),
-                                tooltip: 'Search',
-                                onPressed: () => showMessage('IconButton'),
-                              ),
-                              FloatingActionButton(
-                                child:Icon(Icons.print),
-                                onPressed: () => showDialog(
-                                    context:context,
-                                    builder:(context) {
-                                      return AlertDialog(content: Text(myController.text));
-                                    }
-                                )
-                              )
-                            ],
-                          ),
-                          SizedBox(height: 0), //여백
-                          TextField(
-                            controller: myController,
-                            decoration: InputDecoration(
-                                border: InputBorder.none,
-                                hintText: '샌프란시스코'
-                            ),
-                            style: TextStyle(
-                                color: Colors.white54,
-                                fontSize: 30,
-                                fontWeight: FontWeight.w500)
-                          ),
-                        ],
-                      )))
-            ],
-          )),
+            Expanded(
+                child: Container(
+                    height: 200,
+                    width: width,
+                    decoration: BoxDecoration(
+                      color: LightColor.purple,
+                    ),
+                    child: Stack(
+                      fit: StackFit.expand,
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        Positioned(
+                            top: 30,
+                            right: -100,
+                            child: _circularContainer(300, LightColor.lightpurple)),
+                        Positioned(
+                            top: -100,
+                            left: -45,
+                            child: _circularContainer(width * .5, LightColor.darkpurple)),
+                        Positioned(
+                            top: -180,
+                            right: -30,
+                            child: _circularContainer(width * .7, Colors.transparent,
+                                borderColor: Colors.white38)),
+                        Positioned(
+                            top: 40,
+                            left: 0,
+                            child: Container(
+                                width: width,
+                                padding: EdgeInsets.symmetric(horizontal: 20),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.keyboard_arrow_left,
+                                      color: Colors.white,
+                                      size: 40,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Row(
+                                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                      children: <Widget>[
+                                        Text(
+                                          "지역을 입력해주세요.",
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontWeight: FontWeight.w500),
+                                        ),
+                                        IconButton(
+                                          icon: Icon(Icons.search),
+                                          tooltip: 'Search',
+                                          onPressed: () => showMessage('IconButton'),
+                                        ),
+                                        FloatingActionButton(
+                                            child:Icon(Icons.print),
+                                            onPressed: () => showDialog(
+                                                context:context,
+                                                builder:(context) {
+                                                  return AlertDialog(content: Text(myController.text));
+                                                }
+                                            )
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(height: 0), //여백
+                                    TextField(
+                                        controller: myController,
+                                        decoration: InputDecoration(
+                                            border: InputBorder.none,
+                                            hintText: '샌프란시스코'
+                                        ),
+                                        style: TextStyle(
+                                            color: Colors.white54,
+                                            fontSize: 30,
+                                            fontWeight: FontWeight.w500)
+                                    ),
+                                  ],
+                                )))
+                      ],
+                    )
+
+                )
+            )
+
+          ],
+
+      ),
     );
   }
 
